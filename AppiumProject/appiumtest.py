ab703cc0410517bdb2ba7e7151111e67088698ff
One2ThreeZJM <253521624@qq.com>
2016-11-03 10:47:57 +0800

    test

--- AppiumProject/appiumtest.py
+++ AppiumProject/appiumtest.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 from appium import webdriver
 import time
-
+# aa
 # device=mr.waitForConnection(5,"047fa8290024760d")
 # device.startActivity(component="com.yidejia.app.mall/.ui.MainActivity")
 
