0e0c355c2911f4d47331755e07e2f2c7f0003b50
One2ThreeZJM <253521624@qq.com>
2016-11-04 14:03:07 +0800

    增加内容

--- AppiumProject/PO/LoginPage.py
+++ AppiumProject/PO/LoginPage.py
@@ -15,38 +15,40 @@ class LoginPage(Base):
 	btn_switch_loc = (By.ID,"com.yidejia.app.mall:id/switch_login_status")
 	btn_login_loc = (By.ID,"com.yidejia.app.mall:id/btn_login_now")
 	btn_regist_loc = (By.ID,"com.yidejia.app.mall:id/tv_toolbar_menu")
+	
 	btn_user_loc = (By.CLASS_NAME,"android.widget.RelativeLayout")
 	
 	def click_user(self):
 		'''应用启动默认在首页位置，需要点击用户按钮才到登录页面'''
-		print('点击个人信息按钮')
-		userbtn = self.find_elements(self.btn_user_loc)[-1].click()
-		print('跳转到：'+self.find_element(self.title_loc).text+'页面')
+		# print('点击个人信息按钮')
+		self.find_elements(self.btn_user_loc)[-1].click()
+		# print('跳转到：'+self.page_title()+'页面')
 
+	def page_title(self):
+		return self.find_element(self.title_loc).text
 
-
-	def login(self,account,pwd):
-		self.click_user()
+	def input_account(self,account):
 		self.send_keys(self.edt_account_loc,account,click_first=False)
-		self.send_keys(self.edt_pwd_loc,pwd,click_first=False)
+
+	def input_password(self,password):
+		self.send_keys(self.edt_pwd_loc,password,click_first=False)
+
+	def click_toggle(self):
 		self.clickBtn(self.btn_toggle_loc)
+
+	def click_switch(self):
 		self.clickBtn(self.btn_switch_loc)
+
+	def click_login(self):
 		self.clickBtn(self.btn_login_loc)
+
+	def click_forgot(self):
+		self.clickBtn(self.btn_forgot_loc)
+
+	def click_regist(self):
+		self.clickBtn(self.btn_regist_loc)
+	
 		# 测试登录判断：输入不符合逻辑的account，能不能按登录键
 		# 输入账号、密码。是否可以跳转。判断页面是否跳转、或者按钮能不能按
 		# self.quit()
 
-# desired_caps = {}
-# desired_caps['device'] = 'android'
-# desired_caps['platformName'] = 'android'
-# desired_caps['platformVersion'] = '6.0.1'
-# desired_caps['deviceName'] = '255f4e4c'
-# desired_caps['appPackage'] = 'com.yidejia.app.mall'
-# desired_caps['appActivity'] = 'com.yidejia.app.mall.ui.WelcomeActivity'
-# # desired_caps['appPackage'] = 'com.yidejia.teacher'
-# # desired_caps['appActivity'] = '.ui.MainActivity'	
-# driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
-
-# lp = LoginPage(driver)
-# time.sleep(5)
-# lp.login('1000000000','222222')
--- AppiumProject/TestCase/Test_login.py
+++ AppiumProject/TestCase/Test_login.py
@@ -16,21 +16,36 @@ class Login(unittest.TestCase):
 		desired_caps['platformName'] = 'android'
 		desired_caps['platformVersion'] = '6.0.1'
 		desired_caps['deviceName'] = '255f4e4c'
-		# desired_caps['deviceName'] = 'emulator-5554'
 		desired_caps['appPackage'] = 'com.yidejia.app.mall'
 		desired_caps['appActivity'] = 'com.yidejia.app.mall.ui.WelcomeActivity'
+		# desired_caps['deviceName'] = 'emulator-5554'
 		# desired_caps['appPackage'] = 'com.yidejia.teacher'
 		# desired_caps['appActivity'] = '.ui.MainActivity'
 		# desired_caps['appPackage'] = 'com.android.calculator2'
 		# desired_caps['appActivity'] = '.Calculator'	
 		driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
-		time.sleep(3)
+		
+		time.sleep(5)
 		self.lp = LoginPage(driver)
 
+		self.temp = [['',''],['1',''],['','1'],['12345678911','12345'],['123456789123','123456']]
 
-	def test_login(self):
+	def test_login_fail(self):
 		'''测试登录功能'''
-		pass
+		self.lp.click_user()
+		self.assertEqual(self.lp.page_title(),'登录')
+
+		for x in self.temp:
+			self.lp.input_account(x[0])
+			self.lp.input_password(x[1])
+			self.lp.click_login()
+			self.assertEqual(self.lp.page_title(),'登录')
+
+		
+
+		
+
+
 	
 	def tearDown(self):
 		self.lp.quit()
