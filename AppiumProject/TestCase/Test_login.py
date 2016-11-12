f05b7c7c33df9fc136199521efb98fe6eabb281e
One2ThreeZJM <253521624@qq.com>
2016-11-12 14:46:07 +0800

    fix

--- AppiumProject/TestCase/Test_login.py
+++ AppiumProject/TestCase/Test_login.py
@@ -26,26 +26,36 @@ class Login(unittest.TestCase):
 		# desired_caps['appActivity'] = '.Calculator'	
 		# driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
 		
-		# time.sleep(5)
-		# self.lp = LoginPage(driver)
+		time.sleep(5)
+		self.lp = LoginPage(driver)
 
 		self.temp = [['',''],['19900000001',''],['','ydj998'],['1990000000','12345'],['19900000002','123456'],
 		['19900000001','123456 7'],['199000000 2','123456']]
 
 	def test_login_fail(self):
 		'''测试登录功能'''
-		# self.lp.click_user()
-		# self.assertEqual(self.lp.page_title(),'登录')
+		self.lp.click_user()
+		self.assertEqual(self.lp.page_title(),'登录')
 
-		# for x in self.temp:
-		# 	self.lp.input_account(x[0])
-		# 	self.lp.input_password(x[1])
-		# 	self.lp.click_login()
-		# 	self.assertEqual(self.lp.page_title(),'登录')
-		print(self.temp)
+		for x in self.temp:
+			self.lp.input_account(x[0])
+			self.lp.input_password(x[1])
+			self.lp.click_login()
+			self.assertEqual(self.lp.page_title(),'登录')
+
+	def test_login_success(self):
+		'''测试登录功能'''
+		self.lp.click_user()
+		self.assertEqual(self.lp.page_title(),'登录')
+
+		self.lp.input_account('19900000001')
+		self.lp.input_password('123456789')
+		self.lp.click_login()
+		self.assertEqual(self.lp.page_title(),'NotFound')
+		
 
 
 	def tearDown(self):
-		# self.lp.quit()
-		print('test_login_fail_tearDown')
+		self.lp.quit()
+		# print('test_login_fail_tearDown')
 
